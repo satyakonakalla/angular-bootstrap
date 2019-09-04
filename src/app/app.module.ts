dd129ad6428eb927c36cacd7ecccda16834801cd
Satya K <satyan.konakalla@gmail.com>
2019-09-04 11:39:47 +0530

    merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
+
 import { NgbModalModule, NgbDatepickerModule  } from '@ng-bootstrap/ng-bootstrap';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule, NgbDatepickerModule.forRoot(), FormsModule
+    BrowserModule, NgbDatepickerModule.forRoot(), FormsModule, UiModule
     
   ],
   providers: [],
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './layout/layout.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -10,22 +7,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DtrLibraryComponent } from './datepicker/datepicker-popup';
 import { CalendarComponent } from './datepicker/dtr-calendar.component';
 import { NgbModalModule, NgbDatepickerModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgbMomentjsAdapter } from './moment/ngb-momentjs-adapter';
 import {HolidayService} from './holidays/services/holiday.service';
 import { BillpaymentsComponent } from './billpayments/billpayments.component';
 import { BillslistComponent } from './billslist/billslist.component';
-
-
-
+import { LayoutComponent} from './layout/layout.component'
 
 @NgModule({
   imports: [
     CommonModule, FormsModule, ReactiveFormsModule, NgbModalModule.forRoot(), NgbModule.forRoot(),
     NgbDatepickerModule.forRoot()
   ],
-  declarations: [LayoutComponent, HeaderComponent, FooterComponent, DtrLibraryComponent, 
+  declarations: [ DtrLibraryComponent, LayoutComponent, 
     CalendarComponent, BillpaymentsComponent, BillslistComponent],
-  providers: [NgbMomentjsAdapter, HolidayService],
+  providers: [HolidayService],
   exports: [LayoutComponent]
 })
 export class UiModule { }
